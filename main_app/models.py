4d7762fd93e7d56243e872f1446154085cd5acef
cameron <aliyevcameron@gmail.com>
2024-08-15 10:11:51 -0400

    finish 3th parth

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-from django.db import models
+from django.urls import reverse
 
 class Cat(models.Model):
     name = models.CharField(max_length=100)
@@ -10,3 +10,6 @@ class Cat(models.Model):
     
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('cat-detail', kwargs={'cat_id': self.id})
